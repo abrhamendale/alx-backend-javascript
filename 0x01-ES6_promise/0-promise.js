18e33f68513972a0b726a884de72b81b2602386a
Abrham Endale <tmailae2020@gmail.com>
2023-04-21 13:53:37 +0300

    Fixed permission issues for 0-promise.

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,9 @@
-function getResponseFromAPI(){
-	const ret = new Promise(function(resolve, reject) {
-		  resolve(1);
-	});
-	return ret;
+export function getResponseFromAPI(){
+	  return new Promise((resolve, reject) => {
+		      const xhr = new XMLHttpRequest();
+		      xhr.open("GET", url);
+		      xhr.onload = () => resolve(xhr.responseText);
+		      xhr.onerror = () => reject(xhr.statusText);
+		      xhr.send();
+		    });
 }
