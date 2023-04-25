a1fac289c5e0cd715504d0a0d6c0963c2b3e08e6
Abrham Endale <abrhamendale9@gmail.com>
2023-04-25 21:17:47 +0000

    Fixed 0-get_list_students.js.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
-function getListStudents(){
-	let a = [];
-	a.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-	a.push({ id: 2, firstName: 'James', location: 'Columbia' });
-	a.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
-	return a;
+function getListStudents() {
+  const a = [];
+  a.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  a.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  a.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  return a;
 }
 module.exports = getListStudents;
