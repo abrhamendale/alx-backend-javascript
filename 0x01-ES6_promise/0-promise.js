5c004e1036b08ebcc74ff953bcd640150198d788
Abrham Endale <abrhamendale9@gmail.com>
2023-04-28 22:34:10 +0000

    Fixed eslint issues.

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
-function getResponseFromAPI(){
-	  return new Promise((resolve, reject) => {
-		      resolve(1);
-		    });
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve(1);
+    reject(Error('No rejection'));
+  });
 }
 module.exports = getResponseFromAPI;
