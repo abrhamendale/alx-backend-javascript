fc25159fa1be91a53857c3e1b6d9c75040229068
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 18:28:07 +0300

    Fixed task_8.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 function cleanSet(s, startString) {
-  let str = "";
-  let res = "";
+  let str = '';
+  let res = '';
   s.forEach((value) => {
-    if (value && value.startsWith(startString) && startString != "") {
-      res = str.concat(value.substring(3)+"-");
+    if (value && value.startsWith(startString) && startString !== '') {
+      res = str.concat(value.substring(3) + '-');
       str = res;
     }
-  })
+  });
   str = res.substring(0, res.length - 1);
   return str;
 }
