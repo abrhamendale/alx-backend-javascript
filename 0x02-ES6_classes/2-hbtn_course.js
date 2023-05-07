122ec4e69a02f8e560b5efa07313ff4702a3514c
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 09:58:49 +0300

    A fix for task_2.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,17 +1,26 @@
 export default class HolbertonCourse {
-  _name,
-  _length,
-  _students
+  _name;
+  _length;
+  _students;
   constructor(name, length, students) {
     if (typeof name !== 'string')
       throw new TypeError("Name must be a string");
+    else
+      this._name = name;
     if (typeof length !== 'number')
       throw new TypeError("Length must be a number");
+    else
+      this._length = length;
     if (!Array.isArray(students))
       throw new TypeError("Students must be an array of strings");
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    else
+      if (students[0])
+        if (typeof students[0] == 'string')
+	  this._students = students;
+	else
+	  throw new TypeError("Students must be an array of strings");
+      else
+	  this._students = students;
 }
   get name() {
     return this._name;
@@ -19,6 +28,7 @@ export default class HolbertonCourse {
   set name(x) {
     if (typeof x !== 'string')
       throw new TypeError("Name must be a string");
+    else
       this._name = x;
   }
   get length() {
@@ -27,13 +37,22 @@ export default class HolbertonCourse {
   set length(x) {
     if (typeof x !== 'number')
       throw new TypeError("Length must be a number");
+    else
       this._length = x;
   }
   get students() {
     return this._students;
   }
   set students(x) {
-    console.assert(typeof students === ['String']);
-    this._students = x;
+    if (!Array.isArray(x))
+      throw new TypeError("Students must be an array of strings");
+    else
+      if (x[0])
+	if (typeof x[0] == 'string')
+	  this._students = x;
+	else
+          throw new TypeError("Students must be an array of strings");
+      else
+        this._students = x;
   }
 }
---
+++ 0x02-ES6_classes/2-hbtn_course.setter_type.test.js
@@ -0,0 +1,14 @@
+import HolbertonCourse from "./2-hbtn_course.js";
+
+test("HolbertonCourse checks setter types", () => {
+	    const course = new HolbertonCourse("PHP", 20, ["Lucie", "Guillaume"]);
+	    expect(() => {
+		          course.name = 10;
+		        }).toThrowError();
+	    expect(() => {
+		          course.length = '10';
+		        }).toThrowError();
+	    expect(() => {
+		          course.students = 'Thomas';
+		        }).toThrowError();
+});
