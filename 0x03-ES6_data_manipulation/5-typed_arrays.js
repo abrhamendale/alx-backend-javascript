e269f97a79e43b4cd419c13851b1431b699d186a
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 17:45:34 +0300

    A fix for task_5.

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 function createInt8TypedArray(length, position, value){
-  const view;
-  const buffer;
+  let view = 0;
+  let buffer = 0;
   if (Number.isNaN(length) || Number.isNaN(position) || Number.isNaN(value)) {
     throw Error('Position outside range');
   }
@@ -10,7 +10,7 @@ function createInt8TypedArray(length, position, value){
   }
   if ((!Number.isNaN(length) && !Number.isNaN(position)
     && !Number.isNaN(value)) && (position <= length)) {
-    const buffer = new ArrayBuffer(length);
+    buffer = new ArrayBuffer(length);
     view = new DataView(buffer);
     view.setInt8(position, value);
     return view;
