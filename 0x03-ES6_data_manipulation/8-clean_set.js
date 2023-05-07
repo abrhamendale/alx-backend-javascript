465ba6b34e1bbd88cd96b85556ab6d9183d24e81
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 18:25:17 +0300

    Fixed task_8.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ function cleanSet(s, startString) {
   let str = "";
   let res = "";
   s.forEach((value) => {
-    if (value.startsWith(startString) && startString != "") {
+    if (value && value.startsWith(startString) && startString != "") {
       res = str.concat(value.substring(3)+"-");
       str = res;
     }
