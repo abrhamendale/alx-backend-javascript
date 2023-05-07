c7fbd5799d6e37a2825341c4c9c245ccf54899c0
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 17:52:48 +0300

    A fix for task_6.

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,10 +1,10 @@
-function setFromArray(a){
-	if (Array.isArray(a)){
-		let mySet = new Set(a);
-		return mySet;
-	}
-	else{
-		return ("Set {}");
-	}
+function setFromArray(a) {
+  if (Array.isArray(a)) {
+    let mySet = new Set(a);
+    return mySet;
+  }
+  else {
+    return ("Set {}");
+  }
 }
 module.exports = setFromArray;
