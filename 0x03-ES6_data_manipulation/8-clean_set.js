8cdb9a96cf7f358e9520c6f23c803a07d2364217
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 18:07:52 +0300

    A fix for task_8.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
-function cleanSet(s, startString){
-	var str = "";
-	var res = ""
-	s.forEach(function(value){
-		if (value.startsWith(startString) && startString != ""){
-			res = str.concat(value.substring(3)+"-");
-			str = res;
-		}
-	})
-	str = res.substring(0, res.length - 1);
-	return str;
+function cleanSet(s, startString) {
+  let str = "";
+  let res = "";
+  s.forEach((value) => {
+    if (value.startsWith(startString) && startString != "") {
+      res = str.concat(value.substring(3)+"-");
+      str = res;
+    }
+  })
+  str = res.substring(0, res.length - 1);
+  return str;
 }
 module.exports = cleanSet;
