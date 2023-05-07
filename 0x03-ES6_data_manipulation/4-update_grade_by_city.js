9be352ab35f674913800c4b91a3844a13fd4cbc5
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 13:19:30 +0300

    Added task_4.

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,25 +1,25 @@
-function grader(x, newGrades){
-	var br = 0;
-	var gr = 0;
-	newGrades.forEach(function(element){
-		if (x.id == element.studentId){
-			x.grade = element.grade;
-			br = 1;
-		}
-		else{
-			if (br == 0){
-				x.grade = "N/A";
-			}
-		}
-	});
-	return x;
+function grader(x, newGrades) {
+  var br = 0;
+  var gr = 0;
+  newGrades.forEach(function(element) {
+    if (x.id == element.studentId) {
+      x.grade = element.grade;
+      br = 1;
+    }
+    else {
+      if (br == 0) {
+        x.grade = "N/A";
+      }
+    }
+  });
+  return x;
 }
-function updateStudentGradeByCity(students, city, newGrades){
-	if (!Array.isArray(students)){
-		return [];
-	}
-	const cs = students.filter(x => x.location == city);
-	const mp = cs.map((currentValue, index, array) => grader(currentValue, newGrades));
-	return mp;
+function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  const cs = students.filter((x) => x.location == city);
+  const mp = cs.map((currentValue, index, array) => grader(currentValue, newGrades));
+  return mp;
 }
 module.exports = updateStudentGradeByCity;
