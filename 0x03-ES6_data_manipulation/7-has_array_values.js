9730791b853ebc44fb28c6d1e747f4fac7324b06
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 18:01:32 +0300

    A fix for task_7.

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,18 +1,18 @@
-function hasValuesFromArray(s, a){
-	if (!Array.isArray(a) || !(s instanceof Set)){
-		return false;
-	}
-	var b = 1;
-	a.forEach(function(element){
-		if (!s.has(element)){
-			b = 0;
-		}
-	});
-	if (b){
-		return true;
-	}
-	else{
-		return false;
-	}
+function hasValuesFromArray(s, a) {
+  if (!Array.isArray(a) || !(s instanceof Set)) {
+    return false;
+  }
+  let b = 1;
+  a.forEach((element) => {
+    if (!s.has(element)) {
+      b = 0;
+    }
+  });
+  if (b) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 module.exports = hasValuesFromArray;
