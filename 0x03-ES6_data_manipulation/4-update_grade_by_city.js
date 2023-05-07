5891cb4042984c4cd9dc888bc7d31a1207e76b15
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 13:34:37 +0300

    Added task_4.

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,13 @@
 function grader(x, newGrades) {
-  var br = 0;
-  var gr = 0;
-  newGrades.forEach(function(element) {
-    if (x.id == element.studentId) {
+  let br = 0;
+  newGrades.forEach(function f(element) {
+    if (x.id === element.studentId) {
       x.grade = element.grade;
       br = 1;
     }
     else {
-      if (br == 0) {
-        x.grade = "N/A";
+      if (br === 0) {
+        x.grade = 'N/A';
       }
     }
   });
@@ -18,8 +17,8 @@ function updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(students)) {
     return [];
   }
-  const cs = students.filter((x) => x.location == city);
-  const mp = cs.map((currentValue, index, array) => grader(currentValue, newGrades));
+  const cs = students.filter((x) => x.location === city);
+  const mp = cs.map((currentValue) => grader(currentValue, newGrades));
   return mp;
 }
 module.exports = updateStudentGradeByCity;
