8256c29084ed8192f20b9ba0a1a0c43544a1456c
Abrham Endale <tmailae2020@gmail.com>
2023-05-07 10:12:25 +0300

    Added task_3.

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,23 @@
-class Currency {
-	_code;
-	_name;
-	constructor(code, name) {
-		this._code = code;
-		this._name = name;
-	}
-
-	set code(x) {
-		this._code = x;
-	}
-
-	get code() {
-		return this._code;
-	}
-
-	set name(x) {
-		this._name = name;
-	}
-
-	get name() {
-		return this._name;
-	}
-
-	displayFullCurrency() {
-		return this._name (this._code)
-	}
+export default class Currency {
+  _code;
+  _name;
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+  set code(x) {
+    this._code = x;
+  }
+  get code() {
+    return this._code;
+  }
+  set name(x) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  displayFullCurrency() {
+    return this._name+" ("+this._code+")"
+  }
 }
