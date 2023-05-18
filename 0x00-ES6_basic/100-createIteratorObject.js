53e2c425f2f6c50ec269b24c633ab341d6b91b08
Abrham Endale <abrhamendale9@gmail.com>
2023-05-18 11:30:19 +0000

    Fixed lint errors.

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
 export default function createIteratorObject(report) {
-	let ret = [];
-	for (const k in Object.values(report.allEmployees)){
-		console.log(Object.values(k[0]));
-		ret.push(Object.values(k));
-	}
+  const ret = [];
+  for (const k in Object.values(report.allEmployees)) {
+    console.log(Object.values(k[0]));
+    ret.push(Object.values(k));
+  }
   return ret;
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
 export default function returnHowManyArguments(...Rarg) {
-	return Rarg.length;
+  return Rarg.length;
 }
