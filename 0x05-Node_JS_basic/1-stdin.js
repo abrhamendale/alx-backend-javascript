2bb9407360c2abdb4134361e2d38414873024c01
Abrham Endale <abrhamendale9@gmail.com>
2023-06-13 21:45:54 +0000

    Corrections for task_1.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString()}This important software is now closing`);
-  // process.stdout.write('This important software is now closing\n');
-  process.exit();
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
